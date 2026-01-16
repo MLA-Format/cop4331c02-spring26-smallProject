55bde17d59e9ea805931d2f9dc460cfe5f2f1b2f
MLA-Format <122707566+MLA-Format@users.noreply.github.com>
2026-01-15 22:33:46 -0500

    Added sendResultInfoAsJson

--- LAMPAPI/login.php
+++ LAMPAPI/login.php
@@ -6,6 +6,12 @@
 		return json_decode(file_get_contents('php://input'), true);	
 	}
 
-	
+
+	# TODO: Add comment and figure out what this does.
+	function sendResultInfoAsJson($obj) : void
+	{
+		header('Content-type: applicaton/json');
+		echo $obj;
+	}
 
 ?>
