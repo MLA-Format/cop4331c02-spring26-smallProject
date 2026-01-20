c4535f4ed2d2cfa31e93530dffcef486ba3bc882
MLA-Format <122707566+MLA-Format@users.noreply.github.com>
2026-01-20 18:24:34 -0500

    Completed logout function

--- js/main.js
+++ js/main.js
@@ -10,6 +10,12 @@ function login() {
 }
 
 function logout() {
+	userId = 0;
+	firstName = "";
+	lastName = "";
+
+	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	window.location.href = "index.html";
 }
 
 // Saves a cookie that will be used for user validation.
