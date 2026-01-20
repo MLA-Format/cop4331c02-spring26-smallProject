d51d0022fb9be5400c3db4914b0457325e700ff0
MLA-Format <122707566+MLA-Format@users.noreply.github.com>
2026-01-20 18:05:39 -0500

    Added function prototypes

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// TODO: Add actual prefix
+const urlPrefix = 'http://x/LAMPAPI';
+const extension = 'php';
+
+let userId = 0;
+let firstName = "";
+let lastName = "";
+
+function login() {
+}
+
+function logout() {
+}
+
+function saveCookie() {
+}
+
+function readCookie() {
+}
+
+function addUser() {
+}
+
+function addContact() {
+}
+
+function searchContact() {
+}
+
+function deleteContact() {
+}
+
+function editContact() {
+}
+
+
