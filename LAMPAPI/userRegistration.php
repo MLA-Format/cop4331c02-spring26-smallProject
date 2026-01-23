45a14b07e79a8ae84339d25a5f3ed57346ef0285
MLA-Format <122707566+MLA-Format@users.noreply.github.com>
2026-01-22 19:54:12 -0500

    Format fix

--- LAMPAPI/userRegistration.php
+++ LAMPAPI/userRegistration.php
@@ -12,7 +12,9 @@
 	$password = "";
 	
 
+	# This function returns the json response.
 	function getRequestInfo() : stdClass
 	{
 		return json_decode(file_get_contents('php://input'), true);
+	}
 ?>
