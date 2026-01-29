be4d4a019fe150e074d3aad0d8b1a2c1d9627b49
MLA-Format <122707566+MLA-Format@users.noreply.github.com>
2026-01-29 10:56:31 -0500

    Fix for .env error and import paths

--- LAMPAPI/userRegistration.php
+++ LAMPAPI/userRegistration.php
@@ -1,21 +1,25 @@
 <?php
-	require_once __DIR__ . '/vendor/autoload.php';
+	# File imports.
+	require_once __DIR__ . '/../vendor/autoload.php';
+	require_once __DIR__ . '/_sanitizeErrorOut.php';
+	require_once __DIR__ . '/_returnResponseAsJson.php';
+
+	# .env setup.
 	$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 	$dotenv->load();
 
-	require_once '_returnWithError.php';
-
 	$inData = getRequestInfo();
 
-	# TODO: Populate env vals.
-	$dbConn = new mysqli(getenv("HOST"), getenv("USER"), getenv("PASS"), getenv("TBLE"));
+
+	$dbConn = new mysqli($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASS"], $_ENV["DB_TBLE"]);
+	# If statement to check for an errors logging into the db.
 	if ($dbConn->connect_error)
 	{
 		returnResponseAsJson(err: $dbConn->connect_error);
 	}
 	else
 	{
-		$sqlStatement = $dbConn->prepare(getenv("INSERT_USER_SQL"));
+		$sqlStatement = $dbConn->prepare($_ENV["INSERT_USER_SQL"]);
 		# TODO: Add password hashing.
 		$sqlStatement->bind_param("ssss", $inData["firstNameRef"], $inData["lastNameRef"], $inData["username"], $inData["password"]);
 		$sqlStatement->execute();
