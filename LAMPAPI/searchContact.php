330af35f67bfc48f1a42de8c8f50f971c793eaa6
adnanhossain <adnan400283@gmail.com>
2026-02-10 11:44:05 -0500

    Update search to be partial search

--- LAMPAPI/searchContact.php
+++ LAMPAPI/searchContact.php
@@ -13,16 +13,15 @@
 	$searchResults = array();
 
 	# Check if required fields exist in request
-	if (!isset($inData["userID"]) || !isset($inData["firstName"]) || !isset($inData["lastName"]))
+	if (!isset($inData["userID"]) || !isset($inData["name"]))
 	{
-		$errorRes = array("userID" => 0, "firstName" => "", "lastName" => "");
+		$errorRes = array("userID" => 0, "name" => "");
 		returnResponseAsJson(sanitizeErrorOut($errorRes), "MISSING_SEARCH_PARAMETERS");
 		return;
 	}
 
 	$userID = $inData["userID"];
-	$firstName = $inData["firstName"];
-	$lastName = $inData["lastName"];
+	$name = $inData["name"];
 
 	# Initializing database connection.
 	$conn = new mysqli($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASS"], $_ENV["DB_TBLE"]);
@@ -30,23 +29,26 @@
 	# If statement used to validate the connection.
 	if ($conn->connect_error)
 	{
-		returnResponseAsJson(sanitizeErrorOut(compact('userID', 'firstName', 'lastName')), $conn->connect_error);
+		returnResponseAsJson(sanitizeErrorOut(compact('userID', 'name')), $conn->connect_error);
 	} else {
-		# Running SQL statement to search by firstName and lastName for specific userID
-		$sqlStatement = $conn->prepare("SELECT ID, firstName, lastName, phone, email, userID FROM Contacts WHERE userID = ? AND firstName = ? AND lastName = ?");
+		# Running SQL statement to search by concatenated firstName and lastName for specific userID
+		$sqlStatement = $conn->prepare("SELECT ID, firstName, lastName, phone, email, userID FROM Contacts WHERE userID = ? AND CONCAT(firstName, lastName) LIKE ?");
 		
 		if (!$sqlStatement) {
-			returnResponseAsJson(sanitizeErrorOut(compact('userID', 'firstName', 'lastName')), "PREPARE_FAILED: " . $conn->error);
+			returnResponseAsJson(sanitizeErrorOut(compact('userID', 'name')), "PREPARE_FAILED: " . $conn->error);
 			return;
 		}
 
-		if (!$sqlStatement->bind_param("iss", $userID, $firstName, $lastName)) {
-			returnResponseAsJson(sanitizeErrorOut(compact('userID', 'firstName', 'lastName')), "BIND_FAILED: " . $sqlStatement->error);
+		# Add wildcards to search string for partial match
+		$searchName = "%" . $name . "%";
+
+		if (!$sqlStatement->bind_param("is", $userID, $searchName)) {
+			returnResponseAsJson(sanitizeErrorOut(compact('userID', 'name')), "BIND_FAILED: " . $sqlStatement->error);
 			return;
 		}
 
 		if (!$sqlStatement->execute()) {
-			returnResponseAsJson(sanitizeErrorOut(compact('userID', 'firstName', 'lastName')), "EXECUTE_FAILED: " . $sqlStatement->error);
+			returnResponseAsJson(sanitizeErrorOut(compact('userID', 'name')), "EXECUTE_FAILED: " . $sqlStatement->error);
 			return;
 		}
 
